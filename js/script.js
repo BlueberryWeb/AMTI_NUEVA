991a4d211f25b37dd7c85f734b1842f0bdc67bf7
BlueberryWeb <blueberryweb7@gmail.com>
2022-06-21 17:01:27 -0500

    index
    faltan animaciones y animación de scroll

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-function menu(){
-    document.getElementById('menu').style.display = 'block';
-    document.getElementById('header').style.display = 'none';
+function GDL(){
+    document.getElementById('GDL').style.display = 'block';
+    document.getElementById('MTY').style.display = 'none';
+    document.getElementById('CDMX').style.display = 'none';
 }
-function cerrar(){
-    document.getElementById('header').style.display = 'block';
-    document.getElementById('menu').style.display = 'none';
+function MTY(){
+    document.getElementById('GDL').style.display = 'none';
+    document.getElementById('MTY').style.display = 'block';
+    document.getElementById('CDMX').style.display = 'none';
 }
-
+function CDMX(){
+    document.getElementById('GDL').style.display = 'none';
+    document.getElementById('MTY').style.display = 'none';
+    document.getElementById('CDMX').style.display = 'block';
+}
 /* RECAPTCHA */
 $('#form').submit(function (event) {
     event.preventDefault();
