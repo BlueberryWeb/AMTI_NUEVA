da1d29b34ff8db427260e95c2f0d559e6c9d7878
BlueberryWeb <blueberryweb7@gmail.com>
2022-07-08 15:33:42 -0500

    carrousel de opiniones

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ function CDMX(){
     document.getElementById('MTY').style.display = 'none';
     document.getElementById('CDMX').style.display = 'block';
 }
+//CARROUSEL DE OPINIONES
+let x = 0;
+let p = -10;
+let i = 1;
+let doom = document.getElementsByClassName('item');
+let confr = (doom.length - 3)*(-350);
+let confl = 0;
+//left
+document.getElementById("l").addEventListener("click", function(){
+       x += 350;
+       if (x > confl){x = confr - 350}else{
+    document.getElementById("move").style.marginLeft = x + "px";}
+});
+//right
+document.getElementById("r").addEventListener("click", function(){
+    x += -350;
+    p += -10;
+    if (x < confr){x = 350}else{
+    document.getElementById("move").style.marginLeft = x + "px";}
+});
 //CAMBIAR IMAGEN CON EL HOVER
 window.addEventListener('load', elige, false);
 function elige() {      
